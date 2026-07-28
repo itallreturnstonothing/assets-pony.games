007c736a455bc450f68d07177eb8b67b3fdcd512
carrot <carrot32@horsefucker.org>
2026-07-28 13:31:55 -0400

    make use of automatic ROWID column

--- schema.sql
+++ schema.sql
@@ -1,6 +1,5 @@
 drop table if exists FILE;
 create table FILE(
-    FILE_ID INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     hash TEXT NOT NULL,
     version INTEGER NOT NULL,
@@ -9,7 +8,6 @@ create table FILE(
 
 drop table if exists LICENSE;
 create table LICENSE(
-    LICENSE_ID INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     create_timestamp INTEGER NOT NULL,
     update_timestamp INTEGER NOT NULL
@@ -17,10 +15,9 @@ create table LICENSE(
 
 drop table if exists ASSET;
 create table ASSET(
-    ASSET_ID INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     description TEXT,
-    LICENSE_ID REFERENCES LICENSE (LICENSE_ID),
+    LICENSE_ID REFERENCES LICENSE (ROWID),
     downloads INTEGER NOT NULL DEFAULT 0,
     create_timestamp INTEGER NOT NULL,
     update_timestamp INTEGER NOT NULL
@@ -28,7 +25,6 @@ create table ASSET(
 
 drop table if exists AUTHOR;
 create table AUTHOR(
-    AUTHOR_ID INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     create_timestamp INTEGER NOT NULL,
     update_timestamp INTEGER NOT NULL
@@ -36,7 +32,6 @@ create table AUTHOR(
 
 drop table if exists ASSET_PACK;
 create table ASSET_PACK(
-    ASSET_PACK_ID INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     description TEXT,
     version TEXT,
@@ -46,7 +41,6 @@ create table ASSET_PACK(
 
 drop table if exists TAG;
 create table TAG(
-    TAG_ID INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL UNIQUE,
     description TEXT,
     create_timestamp INTEGER NOT NULL,
@@ -58,9 +52,8 @@ create table TAG(
 
 drop table if exists REL_FILE_ASSET;
 create table REL_FILE_ASSET(
-    REL_FILE_ASSET_ID INTEGER PRIMARY KEY AUTOINCREMENT,
-    FILE_ID REFERENCES FILE (FILE_ID),
-    ASSET_ID REFERENCES ASSET (ASSET_ID),
+    FILE_ID REFERENCES FILE (ROWID),
+    ASSET_ID REFERENCES ASSET (ROWID),
     is_current INTEGER NOT NULL,
     create_timestamp INTEGER NOT NULL,
     update_timestamp INTEGER NOT NULL
@@ -68,24 +61,24 @@ create table REL_FILE_ASSET(
 
 drop table if exists REL_TAG_ASSET;
 create table REL_TAG_ASSET(
-    TAG_ID REFERENCES TAG (TAG_ID),
-    ASSET_ID REFERENCES ASSET (ASSET_ID),
+    TAG_ID REFERENCES TAG (ROWID),
+    ASSET_ID REFERENCES ASSET (ROWID),
     create_timestamp INTEGER NOT NULL,
     PRIMARY KEY (TAG_ID, ASSET_ID)
 );
 
 drop table if exists REL_ASSET_AUTHOR;
 create table REL_ASSET_AUTHOR(
-    ASSET_ID REFERENCES ASSET (ASSET_ID),
-    AUTHOR_ID REFERENCES AUTHOR (AUTHOR_ID),
+    ASSET_ID REFERENCES ASSET (ROWID),
+    AUTHOR_ID REFERENCES AUTHOR (ROWID),
     create_timestamp INTEGER NOT NULL,
     PRIMARY KEY (ASSET_ID, AUTHOR_ID)
 );
 
 drop table if exists REL_ASSET_ASSET_PACK;
 create table REL_ASSET_ASSET_PACK(
-    ASSET_ID REFERENCES ASSET (ASSET_ID),
-    ASSET_PACK_ID REFERENCES ASSET_PACK (ASSET_PACK_ID),
+    ASSET_ID REFERENCES ASSET (ROWID),
+    ASSET_PACK_ID REFERENCES ASSET_PACK (ROWID),
     create_timestamp INTEGER NOT NULL,
     PRIMARY KEY (ASSET_ID, ASSET_PACK_ID)
 );
