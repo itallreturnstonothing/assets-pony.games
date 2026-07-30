9acff42003e7edf69aede13be2d3b6ebdf42624b
carrot <carrot32@horsefucker.org>
2026-07-30 19:05:51 -0400

    add previews to assets.json

--- make_json.py
+++ make_json.py
@@ -7,6 +7,7 @@ with sqlite3.connect("test.sqlite3") as conn, open("assets.json", "w") as outfil
     lk_authors = dict(cursor.execute("select AUTHOR_ID, name from AUTHOR").fetchall())
     lk_asset_packs = dict(cursor.execute("select ASSET_PACK_ID, name from ASSET_PACK").fetchall())
     lk_download_links = dict(cursor.execute("select DOWNLOAD_LINK_ID, url from DOWNLOAD_LINK").fetchall())
+    lk_previews = dict(cursor.execute("select PREVIEW_ID, unique_file_name from PREVIEW").fetchall())
     
     assets_result = cursor.execute("select ASSET_ID, name from ASSET").fetchall()
     
@@ -16,6 +17,7 @@ with sqlite3.connect("test.sqlite3") as conn, open("assets.json", "w") as outfil
     tags_result = cursor.execute("select ASSET_ID, TAG_ID from REL_TAG_ASSET").fetchall()
     asset_packs_result = cursor.execute("select ASSET_ID, ASSET_PACK_ID from REL_ASSET_ASSET_PACK").fetchall()
     download_links_result = cursor.execute("select ASSET_ID, DOWNLOAD_LINK_ID from REL_ASSET_DOWNLOAD_LINK").fetchall()
+    previews_result = cursor.execute("select ASSET_ID, PREVIEW_ID from REL_ASSET_PREVIEW").fetchall()
     for asset in assets:
         asset_id = asset["id"]
         
@@ -30,5 +32,8 @@ with sqlite3.connect("test.sqlite3") as conn, open("assets.json", "w") as outfil
         
         links = [lk_download_links[link_id] for (row_asset_id, link_id) in download_links_result if row_asset_id == asset_id]
         asset["downloads"] = links
+        
+        previews = ["/asset_previews/" + lk_previews[preview_id] for (row_asset_id, preview_id) in previews_result if row_asset_id == asset_id]
+        asset["previews"] = previews
     
     json.dump(assets, outfile, indent=4)
