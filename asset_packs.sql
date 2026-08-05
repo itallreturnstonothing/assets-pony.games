5f4160e5e11972aea6ad6b5d1df2fa06424a7eda
carrot <carrot32@horsefucker.org>
2026-08-05 15:53:15 -0400

    remove some asset packs
    remove packs that only have one asset in them.

--- asset_packs.sql
+++ asset_packs.sql
@@ -6,13 +6,7 @@ values
 ('Chibi ponies'),
 ('Juice, empty and filled bucket'),
 ('Lyra and bonbon'),
-('Numget'),
 ('Sugar cube corner furniture'),
 ('#41899883'),
-('Celestia walking'),
-('doomguy-esque HUD element'),
-('Golden Oak tileset'),
-('Interior House tileset'),
-('Twi idle and running'),
 ('Pony, expressions, bit and scroll')
 ;
